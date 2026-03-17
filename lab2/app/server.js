f89f9de5014d94df1eb78a4757e2cfc256b07a7e
Sofiia Slavinska <905593@edu.p.lodz.pl>
2026-03-17 23:51:11 +0100

    lab2_1

---
+++ lab2/app/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const version = fs.readFileSync('version.txt', 'utf8').trim();
+
+if (config.mode === 'mode1') {
+    app.use(cors());
+}
+
+app.use((req, res, next) => {
+    if (config.mode === 'csp-strict') {
+        res.setHeader('Content-Security-Policy', "default-src 'self'");
+    }
+    next();
+});
+
+app.use(express.static('public'));
+
+app.get('/api/emails', (req, res) => {
+    res.json([
+        { id: 1, sender: "boss@corp.com", subject: "Urgent", body: "Need those reports." },
+        { id: 2, sender: "hr@corp.com", subject: "Party", body: "Pizza at 12!" }
+    ]);
+});
+
+app.listen(3000, () => {
+    console.log(`[System] Starting ${config.appName} v${version} on Port 3000...`);
+});
---
+++ lab2/cdn/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs'); 
+const app = express();
+
+app.use(cors()); 
+
+const publicPath = path.join(__dirname, 'public');
+console.log("Static folder path: ", publicPath);
+
+app.use(express.static(publicPath));
+
+app.get('/ping', (req, res) => {
+    res.send("The CDN server is alive and receiving requests!");
+});
+
+app.get('/debug', (req, res) => {
+    const cssPath = path.join(publicPath, 'theme.css');
+    const imgPath = path.join(publicPath, 'logo.png');
+    
+    res.json({
+        "Where Node is looking": publicPath,
+        "Can Node see theme.css?": fs.existsSync(cssPath),
+        "Can Node see logo.png?": fs.existsSync(imgPath)
+    });
+});
+
+app.listen(6000, () => console.log('[System] Starting CDN on Port 6000...'));
